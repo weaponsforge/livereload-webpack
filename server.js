a852dfc40d7492e1b0f3ccd232a821dc826ba668
weaponsforge <forgeweapons@gmail.com>
2026-08-08 12:21:05 +0800

    fix: update catch-all route for express v5

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+app.get('/{*path}', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(PORT, () => {
